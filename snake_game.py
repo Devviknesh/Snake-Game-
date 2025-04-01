f902ad0a04089d406d3fd2434ded1aae6284135e
DEV VIKNESH A D <164396475+Devviknesh@users.noreply.github.com>
2025-04-01 22:39:54 +0530

    Update snake_game.py

--- snake_game.py
+++ snake_game.py
@@ -9,7 +9,7 @@ WIDTH, HEIGHT = 600, 400
 win = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Snake Game")
 
-# Colors
+
 WHITE, RED, GREEN, BLACK = (255, 255, 255), (213, 50, 80), (0, 255, 0), (0, 0, 0)
 
 # Clock
